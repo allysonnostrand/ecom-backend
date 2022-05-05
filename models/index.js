9be87cc5620b08cff23cc4239488eaca9c0d7343
iggy <allyson.nostrandd@gmail.com>
2022-05-04 17:17:44 -0700

    created model associations in index.js

--- models/index.js
+++ models/index.js
@@ -8,9 +8,24 @@ const ProductTag = require('./ProductTag');
 
 // Categories have many Products
 
+Product.belongsTo(Category);
+Category.hasMany(Product);
+
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag
+  }
+})
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag
+  }
+})
 
 module.exports = {
   Product,
