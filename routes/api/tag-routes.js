e40c281c51c58e0b93e7f8dc1b172d9eb991fbf6
iggy <allyson.nostrandd@gmail.com>
2022-05-04 20:55:20 -0700

    created crud functions for tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,51 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll(req.params.id, {
+    include: [{}]
+  }).then(dbTags => {
+    res.json(dbTags)
+  })
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag.findByPk(req.params.id, {
+    include: [{}]
+  }).then(dbTag => {
+    res.json(dbTag)
+  })
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body).then(newTag => {
+    res.json(newTag)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(updatedTag => {
+    res.json(updatedTag)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(deleteTag => {
+    res.json(deleteTag)
+  })
 });
 
 module.exports = router;
